54d8ec8eda9fa3431e5a2353aa4b555d7de1cd19
j-choy <143446187+j-choy@users.noreply.github.com>
2023-09-23 22:58:31 -0400

    Minor corrections to dropbox; deleted commented out code

--- solution-hw3/js/app.js
+++ solution-hw3/js/app.js
@@ -1,30 +1,3 @@
-// let glazeOptions = [
-//     {
-//         option: 'original',
-//         text: 'Keep original',
-//     },
-//     {
-//         option: 'sugarmilk',
-//         text: 'Sugar milk',
-//     },
-//     {
-//         option: 'vanillamilk',
-//         text: 'Vanilla milk',
-//     },
-//     {
-//         option: 'doublechocolate',
-//         text: 'Double chocolate',
-//     },
-// ];
-
-// var glazeNames = ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"];
-
-// var glazePricing = [0.00, 0.00, 0.50, 1.50];
-
-// let packSizes = [1, 3, 6, 12];
-
-// let packPricing = [1, 3, 5, 10];
-
 const glazes = {
     names: ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"],
     prices: [0.00, 0.00, 0.50, 1.50],
@@ -78,8 +51,6 @@ let currentPrice = document.querySelector(".details-pricing");
 const basePrice = 2.49;
 
 function glazingChange() {
-    console.log('You selected ' + this.value);
-    console.log(basePrice + this.value);
     let priceChange = parseFloat(this.value);
     let newPrice = (basePrice + priceChange) * packDisplayedName.value;
     newPrice = newPrice.toFixed(2);
@@ -88,8 +59,6 @@ function glazingChange() {
 }
 
 function packChange() {
-    console.log('You selected ' + this.value);
-    console.log(basePrice + this.value);
     let sizeChange = this.value;
     let newPrice = (basePrice + parseFloat(glazeDisplayedName.value)) * sizeChange;
     newPrice = newPrice.toFixed(2);
